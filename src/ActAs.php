785a5b704c2085d1c6474a841cf00f7f620811a1
Toby Griffiths <toby@cubicmushroom.co.uk>
2014-12-10 13:28:02 +0000

    Added ActAs class & interface

---
+++ src/ActAs.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: toby
+ * Date: 09/12/14
+ * Time: 09:36
+ */
+
+namespace CubicMushroom\ActAs;
+
+
+class ActAs implements ActAsInterface
+{
+} 
---
+++ src/ActAsInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: toby
+ * Date: 10/12/14
+ * Time: 13:12
+ */
+
+namespace CubicMushroom\ActAs;
+
+
+interface ActAsInterface
+{
+} 
