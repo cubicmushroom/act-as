508fdb547886922289158f7fbf9171864a42a540
Toby Griffiths <toby@cubicmushroom.co.uk>
2014-12-10 20:38:29 +0000

    Updated setup test with setting storage engine

---
+++ src/StorageEngine/StorageEngine.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: toby
+ * Date: 10/12/14
+ * Time: 20:17
+ */
+
+namespace CubicMushroom\ActAs\StorageEngine;
+
+
+class StorageEngine implements StorageEngineInterface
+{
+} 
--- tests/unit/ActAsTest.php
+++ tests/unit/ActAsTest.php
@@ -3,26 +3,56 @@
 namespace CubicMushroom\ActAs;
 
 
-class ActAsTest extends \Codeception\TestCase\Test
+use Codeception\Specify;
+use Codeception\TestCase\Test;
+use CubicMushroom\ActAs\StorageEngine\StorageEngine;
+
+
+class ActAsTest extends Test
 {
+    use Specify;
+
     /**
      * @var \UnitTester
      */
     protected $tester;
 
+    /**
+     * @var ActAs
+     */
+    protected $actAs;
+
+
+    /**
+     * Run before each test
+     */
     protected function _before()
     {
+        $this->actAs = new ActAs;
     }
 
+
+    /**
+     * Run after each test
+     */
     protected function _after()
     {
     }
 
+
     // tests ActAs sets up OK with correct parameters
     public function testCorrectSetup()
     {
-        $actAs = new ActAs;
-        $actAs->setStorageEngine(null);
+        $this->specify('can set the storage engine to be used', function() {
+            $storageClass = '\CubicMushroom\ActAs\StorageEngine\StorageEngine';
+            /** @var StorageEngine $mockStorageEngine */
+            $mockStorageEngine = $this->getMock($storageClass);
+            $this->actAs->setStorageEngine($mockStorageEngine);
+            $returnedStorageEngine = $this->actAs->getStorageEngine();
+            $this->assertInstanceOf($storageClass, $returnedStorageEngine);
+        });
+
+        $this->specify('can set the validator');
     }
 
     // tests ActAs fails with incorrect storageEngine parameter
